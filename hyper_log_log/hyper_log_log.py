ee15fe1da0ae0d003f3ddcb73da844a57afd3563
Conor D'Arcy <conor0456@gmail.com>
2026-03-13 17:27:05 -0700

    Tests passing

--- hyper_log_log/hyper_log_log.py
+++ hyper_log_log/hyper_log_log.py
@@ -2,41 +2,84 @@
 https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/40671.pdf
 """
 
-from cmath import inf
 import hashlib
 import pickle
+from functools import total_ordering
 
+@total_ordering
 class HyperLogLog:
-    def __init__(self, number_of_registers=50):
-        self.longest_prefix_registers = [0] * number_of_registers
-        self.number_of_registers = number_of_registers
+    HASH_RESULTING_BYTE_SIZE = 8 #64 bits
+    def __init__(self, num_bits_for_register_mapping = 15):
+        assert 0 < num_bits_for_register_mapping < 63, "Number of bits must be within range [1,62]"
+        self.number_of_registers = 2**num_bits_for_register_mapping
+        self.registers = [0] * self.number_of_registers
+        self.num_bits_for_register_mapping = num_bits_for_register_mapping
+        self.alpha = self._alpha()
     
     def update(self, record):
         object_bytes = pickle.dumps(record)
-        hashed_bytes = hashlib.sha256(object_bytes).digest()
-        register = int.from_bytes(hashed_bytes, byteorder='big') % self.number_of_registers
-        self.longest_prefix_registers[register] = max(self.longest_prefix_registers[register], self._count_leading_zeros(hashed_bytes))
+        hashed_bytes = hashlib.md5(object_bytes).digest()[:self.HASH_RESULTING_BYTE_SIZE]
+        hashed_int = int.from_bytes(hashed_bytes, byteorder='big', signed=False)
+        register, remainder = self._split_bits(hashed_int)
+        self.registers[register] = max(self.registers[register], self._count_zeros(remainder))
 
-    def cardinality(self):
-        if sum(self.longest_prefix_registers) < 2:
-            return 0
-        return sum(map(lambda x: (2**x) / self.number_of_registers, self.longest_prefix_registers))
+    def merge(self, other):
+        if not isinstance(other, HyperLogLog):
+            return NotImplemented
+        if not self.num_bits_for_register_mapping == other.num_bits_for_register_mapping: 
+            raise ValueError("Merging HLLs requires registers of equal size!")
+        
+        for i in range(self.number_of_registers):
+            self.registers[i] = max(self.registers[i], other.registers[i])
+        return self
+    
+    def cardinality(self):        
+        return int(self._raw_estimate() * self.alpha)
+
+    def _split_bits(self, hashed_int):
+        p = self.num_bits_for_register_mapping
+        register = hashed_int >> (64 - p)
+        remainder = hashed_int & ((1 << (64 - p)) - 1)
+
+        return register, remainder
+    
+    def _count_zeros(self, remainder):
+        q = 64 - self.num_bits_for_register_mapping 
+        if remainder == 0:
+            return q + 1
+        return (q - remainder.bit_length()) + 1
+    
+    def _raw_estimate(self):
+        Z = sum(2.0 ** (-register) for register in self.registers)
+        return (self.number_of_registers ** 2) / Z
+    
+    def _alpha(self):
+        """
+        Alpha is an emperically determined constant set in relation to our number of registers
+        """
+        if self.number_of_registers == 16:
+            return 0.673
+        elif self.number_of_registers == 32:
+            return 0.697
+        elif self.number_of_registers == 64:
+            return 0.709
+        else:
+            return 0.7213 / (1 + 1.079 / self.number_of_registers)
 
     def __add__(self, other):
-        # TODO (Conor) Allow for merging of HLL instances
-        pass
-    
-    def _count_leading_zeros(self, byte_array):
-        leading_zeros = 0
-        for byte in byte_array:
-            i = 7
-            while i > -1 and not ((byte >> i) & 1):
-                leading_zeros += 1
-                i -= 1
-            if i != -1:
-                return leading_zeros
-        return leading_zeros
-    
-    """
-    probability of seeing n 0s in a row is 2^-(n)
-    """
+        if not isinstance(other, HyperLogLog):
+            return NotImplemented
+        new_hll = HyperLogLog(self.num_bits_for_register_mapping)
+        new_hll.registers = self.registers.copy()
+        new_hll.merge(other)
+        return new_hll
+        
+    def __lt__(self, other):
+        if not isinstance(other, HyperLogLog):
+            return NotImplemented
+        return self.cardinality() < other.cardinality()
+
+    def __eq__(self, other):
+        if not isinstance(other, HyperLogLog):
+            return NotImplemented
+        return self.registers == other.registers
